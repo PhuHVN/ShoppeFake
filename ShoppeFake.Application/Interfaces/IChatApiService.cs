8e7162e86a3251a8fed6677c4dab98df27c2f00d
PhuHo <144872413+PhuHVN@users.noreply.github.com>
2026-08-15 21:07:48 +0700

    hotfix/fix-syntax

--- ShoppeFake.Application/Interfaces/IChatApiService.cs
+++ ShoppeFake.Application/Interfaces/IChatApiService.cs
@@ -4,8 +4,8 @@ namespace ShoppeFake.Application.Interfaces
 {
     public interface IChatApiService
     {
-        Task<bool> SendV1Async(string message, CancellationToken cancellationToken = default);
-        Task<bool> SendV2Async(string conversationId, string message, CancellationToken cancellationToken = default);
+        Task<ChatApiResponse?> SendV1Async(string message, CancellationToken cancellationToken = default);
+        Task<ChatApiResponse?> SendV2Async(string conversationId, string message, CancellationToken cancellationToken = default);
         Task<ChatApiResponse<PagingResponse<ConversationResponse>>?> CustomerGetChatHistoryAsync(int pageIndex, int pageSize, CancellationToken cancellationToken = default);
         Task<ChatApiResponse<PagingResponse<ConversationMessageResponse>>?> GetCursorChatHistoryAsync(string conversationId, string? lastCursor, int limit, CancellationToken cancellationToken = default);
         Task<bool> OrderEventAsync(string conversationId, OrdersRequest request, CancellationToken cancellationToken = default);
